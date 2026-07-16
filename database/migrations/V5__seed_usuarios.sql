977ebe8f3ef663c3c1054c079771b185b72fe8e6
fxbricix <fabriiciobr@outlook.com>
2026-07-16 14:02:25 -0300

    fix: migrations

--- database/migrations/V5__seed_usuarios.sql
+++ database/migrations/V5__seed_usuarios.sql
@@ -1,3 +1,3 @@
-INSERT INTO usuarios (nome_usuario, email, senha_hash, role_id, criado_em, atualizado_em) VALUES
+INSERT INTO usuarios (nome, email, senha_hash, role_id, criado_em, atualizado_em) VALUES
 ('Fabricio Moreira', 'fabricio@fiap.com.br', 'TESTE', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 ('Teste FIAP', 'fiap@fiap.com.br', 'TESTE', 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
