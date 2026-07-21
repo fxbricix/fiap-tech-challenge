126038b9d2756e0b3c2bc86bed799347af681687
fxbricix <fabriiciobr@outlook.com>
2026-07-21 14:08:26 -0300

    fix: seed agora tem password hasehada

--- database/migrations/V5__seed_usuarios.sql
+++ database/migrations/V5__seed_usuarios.sql
@@ -1,3 +1,3 @@
 INSERT INTO usuarios (nome, login, email, senha_hash, role_id, criado_em, atualizado_em) VALUES
-('Fabricio Moreira', 'fabricio', 'fabricio@fiap.com.br', 'TESTE', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('Teste FIAP', 'fiap', 'fiap@fiap.com.br', 'TESTE', 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+('Fabricio Moreira', 'fabricio', 'fabricio@fiap.com.br', '$2a$10$zv3.aio3V2ce7krLFiqCSemob.ZWdHdaFeFB9OABOL2xTzfRsjuLq', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Teste FIAP', 'fiap', 'fiap@fiap.com.br', '$2a$10$zv3.aio3V2ce7krLFiqCSemob.ZWdHdaFeFB9OABOL2xTzfRsjuLq', 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
